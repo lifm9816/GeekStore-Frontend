d6e78d410592a1360b2cf070d5384a761b82bc8e
Luis Martínez <lifm9816@gmail.com>
2025-01-23 23:29:08 -0600

    Avance de temas claro y obscuro

---
+++ src/ui/Themes.jsx
@@ -0,0 +1,21 @@
+export const light = {
+    body: "#D9D9D9",
+    primary: "#19222D",
+    secondary:  "#FF914D",
+    chart_title: "#19222D",
+    chart_price: "#FF914D",
+    add_button: "#19222D",
+    view_button: "#FF914D"
+}
+
+export const dark = {
+    body: "#19222D",
+    primary: "#0E141A",
+    secondary: "#B4FF4D",
+    chart_title: "#FFFFFF",
+    chart_price: "#B4FF4D",
+    add_button: "#19222D",
+    view_button: "#B4FFFD",
+    menu_logo_icon: "#7A94AD",
+    menu_logo_icon_active: "#0E141A"
+}
